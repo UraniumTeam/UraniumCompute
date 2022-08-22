da79cdf8b1472fb6e75f1d81eeb006cf9e3dbd5b
n-dub <ragucoz@gmail.com>
2022-08-22 12:43:12 +0500

    Reformat docs for IDeviceObject

--- NativeSource/UraniumCompute/UnCompute/Backend/IDeviceObject.h
+++ NativeSource/UraniumCompute/UnCompute/Backend/IDeviceObject.h
@@ -10,20 +10,21 @@ namespace UN
     //! All objects related to a compute backend must implement deferred initialization.
     //! For example:
     //! \code{.cpp}
-    //! class IBuffer : public IDeviceObject
-    //! {
-    //! public:
-    //!     virtual void Init(const BufferDesc& desc) = 0;
-    //!     virtual const BufferDesc& GetDesc()       = 0;
-    //! };
+    //!     class IBuffer : public IDeviceObject
+    //!     {
+    //!     public:
+    //!         using DescriptorType = BufferDesc;
+    //!         virtual void Init(const DescriptorType& desc) = 0;
+    //!         virtual const DescriptorType& GetDesc()       = 0;
+    //!     };
     //!
-    //! class Buffer : public DeviceObjectBase<IBuffer>
-    //! {
-    //! public:
-    //!     static ResultCode Create(Buffer** ppBuffer) { ... } // doesn't create any device objects
-    //!     void Init(const BufferDesc& desc) override  { ... } // creates device objects, allocates memory, etc.
-    //!     void Reset() override                       { ... } // releases device objects and memory
-    //! };
+    //!     class Buffer : public DeviceObjectBase<IBuffer>
+    //!     {
+    //!     public:
+    //!         static ResultCode Create(IBuffer** ppBuffer) { ... } // doesn't create any device objects
+    //!         void Init(const BufferDesc& desc) override   { ... } // creates device objects, allocates memory, etc.
+    //!         void Reset() override                        { ... } // releases device objects and memory
+    //!     };
     //! \endcode
     class IDeviceObject : public IObject
     {
