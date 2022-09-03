193faff122ff4e16f7caeb35252517cb31a6f03b
n-dub <ragucoz@gmail.com>
2022-09-03 18:52:21 +0500

    Correct the result code in `LoadCreateDeviceFactoryProc()`

--- NativeSource/UraniumCompute/UnCompute/Acceleration/IDeviceFactory.h
+++ NativeSource/UraniumCompute/UnCompute/Acceleration/IDeviceFactory.h
@@ -78,14 +78,14 @@ namespace UN
         auto resultCode = DynamicLibrary::Create(ppLibrary);
         if (!Succeeded(resultCode))
         {
-            return ResultCode::Fail;
+            return resultCode;
         }
 
         resultCode = (*ppLibrary)->Init(UraniumComputeDllName);
         if (!Succeeded(resultCode))
         {
             UN_Error(false, "Couldn't load {} library", UraniumComputeDllName);
-            return ResultCode::Fail;
+            return resultCode;
         }
 
         resultCode = (*ppLibrary)->GetFunction(CreateDeviceFactoryProcName, pCreateDeviceFactoryProc);
@@ -95,7 +95,7 @@ namespace UN
                      "Couldn't get the entry point named \"{}\" in \"{}\" library",
                      CreateDeviceFactoryProcName,
                      UraniumComputeDllName);
-            return ResultCode::Fail;
+            return resultCode;
         }
 
         return resultCode;
