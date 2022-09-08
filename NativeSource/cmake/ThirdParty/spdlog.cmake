939aadb614f38c8ca5cb4ca2cb5cf29ceeb92cc1
n-dub <ragucoz@gmail.com>
2022-09-08 15:26:42 +0500

    Move `-fPIC` flag to spdlog

--- NativeSource/cmake/ThirdParty/spdlog.cmake
+++ NativeSource/cmake/ThirdParty/spdlog.cmake
@@ -1,2 +1,6 @@
 add_subdirectory("${UN_PROJECT_ROOT}/ThirdParty/spdlog")
 set_target_properties(spdlog PROPERTIES FOLDER "ThirdParty")
+
+if (NOT UN_COMPILER_MSVC)
+    target_compile_options(spdlog PRIVATE -fPIC)
+endif ()
