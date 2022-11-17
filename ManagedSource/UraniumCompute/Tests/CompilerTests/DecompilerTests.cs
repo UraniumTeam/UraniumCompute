2b95d3a73a644bbd73a3bf086212c5585661a395
n-dub <ragucoz@gmail.com>
2022-11-17 21:38:16 +0500

    Inline lambdas in tests

--- ManagedSource/UraniumCompute/Tests/CompilerTests/DecompilerTests.cs
+++ ManagedSource/UraniumCompute/Tests/CompilerTests/DecompilerTests.cs
@@ -27,41 +27,34 @@ public class Tests
     [Test]
     public void Test3()
     {
-        var func = () =>
-        {
-            var index = 9;
-            return (index + index) * index;
-        };
         var expectedResult = "[numthreads(1, 1, 1)] " +
                              "int main(uint3 globalInvocationID : SV_DispatchThreadID) " +
                              "{ int V_0; int V_1; " +
                              "V_0 = 9; " +
                              "V_1 = ((V_0 + V_0) * V_0); " +
                              "return V_1; }";
-        AssertFunc(func, expectedResult);
+        AssertFunc((Func<int>)(() =>
+        {
+            var index = 9;
+            return (index + index) * index;
+        }), expectedResult);
     }
 
     [Test]
     public void Test4()
     {
-        var func = (Span<int> a) => { return a[10]; };
         var expectedResult = "RWStructuredBuffer<int> a : register(u0); " +
                              "[numthreads(1, 1, 1)] " +
                              "int main(uint3 globalInvocationID : SV_DispatchThreadID) { " +
                              "int V_0; " +
                              "V_0 = a[10]; " +
                              "return V_0; }";
-        AssertFunc(func, expectedResult);
+        AssertFunc((Span<int> a) => { return a[10]; }, expectedResult);
     }
 
     [Test]
     public void Test5()
     {
-        var func = (Span<int> a) =>
-        {
-            a[0] = 6;
-            return 0;
-        };
         var expectedResult = "RWStructuredBuffer<int> a : register(u0); " +
                              "[numthreads(1, 1, 1)] " +
                              "int main(uint3 globalInvocationID : SV_DispatchThreadID) { " +
@@ -69,37 +62,32 @@ public class Tests
                              "a[0] = 6; " +
                              "V_0 = 0; " +
                              "return V_0; }";
-        AssertFunc(func, expectedResult);
+        AssertFunc((Span<int> a) =>
+        {
+            a[0] = 6;
+            return 0;
+        }, expectedResult);
     }
 
     [Test]
     public void Test6()
     {
-        var func = (Span<int> a) =>
-        {
-            var index = GpuIntrinsic.GetGlobalInvocationId();
-            a[index.X] *= 2;
-        };
         var expectedResult = "RWStructuredBuffer<int> a : register(u0); " +
                              "[numthreads(1, 1, 1)] " +
                              "void main(uint3 globalInvocationID : SV_DispatchThreadID) { " +
                              "uint3 V_0; " +
                              "V_0 = globalInvocationID; " +
                              "a[V_0.x] = (a[V_0.x] * 2); }";
-        AssertFunc(func, expectedResult);
+        AssertFunc((Span<int> a) =>
+        {
+            var index = GpuIntrinsic.GetGlobalInvocationId();
+            a[index.X] *= 2;
+        }, expectedResult);
     }
 
     [Test]
     public void Test7()
     {
-        var func = () =>
-        {
-            var index = GpuIntrinsic.GetGlobalInvocationId();
-            var x = index.X;
-            var y = index.Y;
-            var z = index.Z;
-            return x + y;
-        };
         var expectedResult = "[numthreads(1, 1, 1)] " +
                              "int main(uint3 globalInvocationID : SV_DispatchThreadID) { " +
                              "uint3 V_0; " +
@@ -113,7 +101,14 @@ public class Tests
                              "V_3 = V_0.z; " +
                              "V_4 = (V_1 + V_2); " +
                              "return V_4; }";
-        AssertFunc(func, expectedResult);
+        AssertFunc((Func<int>)(() =>
+        {
+            var index = GpuIntrinsic.GetGlobalInvocationId();
+            var x = index.X;
+            var y = index.Y;
+            var z = index.Z;
+            return x + y;
+        }), expectedResult);
     }
 
     private static void AssertFunc(Delegate func, string expectedHlslCode)
