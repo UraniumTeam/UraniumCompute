97ce80abbe647ee5cee77ec7bcb5e7f815d47c19
n-dub <ragucoz@gmail.com>
2022-11-18 14:33:58 +0500

    Refactor compiler tests

---
+++ ManagedSource/UraniumCompute/Tests/CompilerTests/DecompilerTests.Instrinsics.cs
@@ -0,0 +1,48 @@
+﻿using UraniumCompute.Compiler.InterimStructs;
+
+namespace CompilerTests;
+
+public partial class DecompilerTests
+{
+    [Test]
+    public void CompilesGetGlobalInvocationId()
+    {
+        var expectedResult = "RWStructuredBuffer<int> a : register(u0); " +
+                             "[numthreads(1, 1, 1)] " +
+                             "void main(uint3 globalInvocationID : SV_DispatchThreadID) { " +
+                             "uint3 V_0; " +
+                             "V_0 = globalInvocationID; " +
+                             "a[V_0.x] = (a[V_0.x] * 2); }";
+        AssertFunc((Span<int> a) =>
+        {
+            var index = GpuIntrinsic.GetGlobalInvocationId();
+            a[index.X] *= 2;
+        }, expectedResult);
+    }
+
+    [Test]
+    public void CompilesIndex3D_GetXYZ()
+    {
+        var expectedResult = "[numthreads(1, 1, 1)] " +
+                             "int main(uint3 globalInvocationID : SV_DispatchThreadID) { " +
+                             "uint3 V_0; " +
+                             "int V_1; " +
+                             "int V_2; " +
+                             "int V_3; " +
+                             "int V_4; " +
+                             "V_0 = globalInvocationID; " +
+                             "V_1 = V_0.x; " +
+                             "V_2 = V_0.y; " +
+                             "V_3 = V_0.z; " +
+                             "V_4 = (V_1 + V_2); " +
+                             "return V_4; }";
+        AssertFunc((Func<int>)(() =>
+        {
+            var index = GpuIntrinsic.GetGlobalInvocationId();
+            var x = index.X;
+            var y = index.Y;
+            var z = index.Z;
+            return x + y;
+        }), expectedResult);
+    }
+}
--- ManagedSource/UraniumCompute/Tests/CompilerTests/DecompilerTests.cs
+++ ManagedSource/UraniumCompute/Tests/CompilerTests/DecompilerTests.cs
@@ -1,13 +1,11 @@
-using Mono.Cecil;
 using UraniumCompute.Compiler.Decompiling;
-using UraniumCompute.Compiler.InterimStructs;
 
 namespace CompilerTests;
 
-public class Tests
+public partial class DecompilerTests
 {
     [Test]
-    public void Test1()
+    public void CompilesLiterals()
     {
         var expectedResult = "[numthreads(1, 1, 1)] " +
                              "int main(uint3 globalInvocationID : SV_DispatchThreadID) " +
@@ -16,7 +14,7 @@ public class Tests
     }
 
     [Test]
-    public void Test2()
+    public void CompilesExplicitReturn()
     {
         var expectedResult = "[numthreads(1, 1, 1)] " +
                              "int main(uint3 globalInvocationID : SV_DispatchThreadID) " +
@@ -25,7 +23,23 @@ public class Tests
     }
 
     [Test]
-    public void Test3()
+    public void CompilesBinaryExpressions()
+    {
+        var expectedResult = "[numthreads(1, 1, 1)] " +
+                             "int main(uint3 globalInvocationID : SV_DispatchThreadID) " +
+                             "{ int V_0; int V_1; " +
+                             "V_0 = 9; " +
+                             "V_1 = (V_0 + (V_0 * V_0)); " +
+                             "return V_1; }";
+        AssertFunc((Func<int>)(() =>
+        {
+            var index = 9;
+            return index + index * index;
+        }), expectedResult);
+    }
+
+    [Test]
+    public void CompilesBinaryExpressions_WithParentheses()
     {
         var expectedResult = "[numthreads(1, 1, 1)] " +
                              "int main(uint3 globalInvocationID : SV_DispatchThreadID) " +
@@ -41,19 +55,17 @@ public class Tests
     }
 
     [Test]
-    public void Test4()
+    public void CompilesBufferParameter_ReadOnly()
     {
         var expectedResult = "RWStructuredBuffer<int> a : register(u0); " +
                              "[numthreads(1, 1, 1)] " +
                              "int main(uint3 globalInvocationID : SV_DispatchThreadID) { " +
-                             "int V_0; " +
-                             "V_0 = a[10]; " +
-                             "return V_0; }";
-        AssertFunc((Span<int> a) => { return a[10]; }, expectedResult);
+                             "return a[10]; }";
+        AssertFunc((Span<int> a) => a[10], expectedResult);
     }
 
     [Test]
-    public void Test5()
+    public void CompilesBufferParameter_WriteOnly()
     {
         var expectedResult = "RWStructuredBuffer<int> a : register(u0); " +
                              "[numthreads(1, 1, 1)] " +
@@ -67,48 +79,6 @@ public class Tests
             a[0] = 6;
             return 0;
         }, expectedResult);
-    }
-
-    [Test]
-    public void Test6()
-    {
-        var expectedResult = "RWStructuredBuffer<int> a : register(u0); " +
-                             "[numthreads(1, 1, 1)] " +
-                             "void main(uint3 globalInvocationID : SV_DispatchThreadID) { " +
-                             "uint3 V_0; " +
-                             "V_0 = globalInvocationID; " +
-                             "a[V_0.x] = (a[V_0.x] * 2); }";
-        AssertFunc((Span<int> a) =>
-        {
-            var index = GpuIntrinsic.GetGlobalInvocationId();
-            a[index.X] *= 2;
-        }, expectedResult);
-    }
-
-    [Test]
-    public void Test7()
-    {
-        var expectedResult = "[numthreads(1, 1, 1)] " +
-                             "int main(uint3 globalInvocationID : SV_DispatchThreadID) { " +
-                             "uint3 V_0; " +
-                             "int V_1; " +
-                             "int V_2; " +
-                             "int V_3; " +
-                             "int V_4; " +
-                             "V_0 = globalInvocationID; " +
-                             "V_1 = V_0.x; " +
-                             "V_2 = V_0.y; " +
-                             "V_3 = V_0.z; " +
-                             "V_4 = (V_1 + V_2); " +
-                             "return V_4; }";
-        AssertFunc((Func<int>)(() =>
-        {
-            var index = GpuIntrinsic.GetGlobalInvocationId();
-            var x = index.X;
-            var y = index.Y;
-            var z = index.Z;
-            return x + y;
-        }), expectedResult);
     }
 
     private static void AssertFunc(Delegate func, string expectedHlslCode)
