be4bddcada6991fb188af7d7c42c492e159e7a38
Dmitry Ishoev <26188825+NetVoobrazhenia@users.noreply.github.com>
2022-12-19 23:35:46 +0500

    Refactored IntrinsicFunctionSymbol and fixed problems with method overloads calls

--- ManagedSource/UraniumCompute/UraniumCompute.Compiler/Decompiling/IntrinsicFunctionSymbol.cs
+++ ManagedSource/UraniumCompute/UraniumCompute.Compiler/Decompiling/IntrinsicFunctionSymbol.cs
@@ -10,25 +10,10 @@ internal sealed class IntrinsicFunctionSymbol : FunctionSymbol
     public override TypeSymbol ReturnType { get; }
     public override TypeSymbol[] ArgumentTypes { get; }
 
-    private static readonly Dictionary<string, IntrinsicFunctionSymbol> functions;
+    private static readonly Dictionary<(string, int), IntrinsicFunctionSymbol> functions;
 
     static IntrinsicFunctionSymbol()
     {
-        (string, IntrinsicFunctionSymbol) CreateIntrinsic(Delegate d)
-        {
-            var method = d.Method;
-            var returnType = TypeResolver.CreateType(method.ReturnType, _ => { });
-            var arguments = new List<TypeSymbol>();
-            if (method.CallingConvention >= CallingConventions.HasThis)
-                arguments.Add(TypeResolver.CreateType(method.GetType(), _ => { }));
-            arguments.AddRange(
-                method.GetParameters()
-                    .Select(x => x.ParameterType)
-                    .Select(x => TypeResolver.CreateType(x, _ => { })));
-            var symbol = new IntrinsicFunctionSymbol(method.Name.ToLower(), returnType, arguments);
-            return ($"{method.DeclaringType!.Name}.{method.Name}", symbol);
-        }
-
         functions = Enumerable.Empty<Delegate>()
             .Append(MathF.Sin)
             .Append(MathF.Cos)
@@ -47,71 +32,89 @@ internal sealed class IntrinsicFunctionSymbol : FunctionSymbol
             .Append(Matrix4x4Int.Transpose)
             .Append(Matrix4x4Uint.Transpose)
             .Select(CreateIntrinsic)
-            .ToDictionary(x => x.Item1, x => x.Item2);
+            .ToDictionary(x => (x.Item1, 1), x => x.Item2);
 
-        CreateMemberIntrinsic<Matrix2x2>(nameof(Matrix2x2.GetDeterminant), "determinant");
-        CreateMemberIntrinsic<Matrix3x3>(nameof(Matrix2x2.GetDeterminant), "determinant");
-        CreateMemberIntrinsic<Matrix4x4>(nameof(Matrix2x2.GetDeterminant), "determinant");
-        CreateMemberIntrinsic<Matrix2x2Int>(nameof(Matrix2x2.GetDeterminant), "determinant");
-        CreateMemberIntrinsic<Matrix3x3Int>(nameof(Matrix2x2.GetDeterminant), "determinant");
-        CreateMemberIntrinsic<Matrix4x4Int>(nameof(Matrix2x2.GetDeterminant), "determinant");
-        CreateMemberIntrinsic<Matrix2x2Uint>(nameof(Matrix2x2.GetDeterminant), "determinant");
-        CreateMemberIntrinsic<Matrix3x3Uint>(nameof(Matrix2x2.GetDeterminant), "determinant");
-        CreateMemberIntrinsic<Matrix4x4Uint>(nameof(Matrix2x2.GetDeterminant), "determinant");
+        var matrixTypes = new List<Type>
+        {
+            typeof(Matrix2x2),
+            typeof(Matrix3x3),
+            typeof(Matrix4x4),
+            typeof(Matrix2x2Int),
+            typeof(Matrix3x3Int),
+            typeof(Matrix4x4Int),
+            typeof(Matrix2x2Uint),
+            typeof(Matrix3x3Uint),
+            typeof(Matrix4x4Uint)
+        };
 
-        CreateMemberCtorIntrinsic<Matrix2x2>();
-        CreateMemberCtorIntrinsic<Matrix3x3>();
-        CreateMemberCtorIntrinsic<Matrix4x4>();
-        CreateMemberCtorIntrinsic<Matrix2x2Int>();
-        CreateMemberCtorIntrinsic<Matrix3x3Int>();
-        CreateMemberCtorIntrinsic<Matrix4x4Int>();
-        CreateMemberCtorIntrinsic<Matrix2x2Uint>();
-        CreateMemberCtorIntrinsic<Matrix3x3Uint>();
-        CreateMemberCtorIntrinsic<Matrix4x4Uint>();
-        CreateMemberCtorIntrinsic<Vector2>();
-        CreateMemberCtorIntrinsic<Vector3>();
-        CreateMemberCtorIntrinsic<Vector4>();
-        CreateMemberCtorIntrinsic<Vector2Int>();
-        CreateMemberCtorIntrinsic<Vector3Int>();
-        CreateMemberCtorIntrinsic<Vector4Int>();
-        CreateMemberCtorIntrinsic<Vector2Uint>();
-        CreateMemberCtorIntrinsic<Vector3Uint>();
-        CreateMemberCtorIntrinsic<Vector4Uint>();
-        
-        
-        var m = new Matrix4x4();
-        var v = new Vector4();
-        Matrix4x4.Lerp(m,m,5);
-        MathF.Acos(5.0f);
-        v.Length();
-        Vector4.Dot(v, v);
+        var vectorTypes = new List<Type>
+        {
+            typeof(Vector2),
+            typeof(Vector3),
+            typeof(Vector4),
+            typeof(Vector2Int),
+            typeof(Vector3Int),
+            typeof(Vector4Int),
+            typeof(Vector2Uint),
+            typeof(Vector3Uint),
+            typeof(Vector4Uint)
+        };
+
+        foreach (var type in matrixTypes)
+        {
+            CreateMemberCtorIntrinsic(type);
+            CreateMemberIntrinsic(type, nameof(Matrix2x2.GetDeterminant), "determinant");
+        }
+
+        foreach (var type in vectorTypes)
+        {
+            CreateMemberCtorIntrinsic(type);
+        }
     }
 
-    private static void CreateMemberIntrinsic<T>(string name, string? hlslName = null)
+    private static (string, IntrinsicFunctionSymbol) CreateIntrinsic(Delegate d)
     {
-        var method = typeof(T).GetMethod(name) ?? throw new ArgumentException();
-        hlslName ??= method.Name.ToLower();
+        var method = d.Method;
         var returnType = TypeResolver.CreateType(method.ReturnType, _ => { });
         var arguments = new List<TypeSymbol>();
         if (method.CallingConvention >= CallingConventions.HasThis)
-            arguments.Add(TypeResolver.CreateType(typeof(T), _ => { }));
+            arguments.Add(TypeResolver.CreateType(method.GetType(), _ => { }));
         arguments.AddRange(
             method.GetParameters()
                 .Select(x => x.ParameterType)
                 .Select(x => TypeResolver.CreateType(x, _ => { })));
-        var symbol = new IntrinsicFunctionSymbol(hlslName, returnType, arguments);
-        functions[$"{method.DeclaringType!.Name}.{method.Name}"] = symbol;
+        var symbol = new IntrinsicFunctionSymbol(method.Name.ToLower(), returnType, arguments);
+        return ($"{method.DeclaringType!.Name}.{method.Name}", symbol);
     }
 
-    private static void CreateMemberCtorIntrinsic<T>()
+    private static void CreateMemberIntrinsic(Type type, string name, string? hlslName = null)
     {
-        var constructors = typeof(T).GetConstructors() ?? throw new ArgumentException();
+        var methods = type.GetMethods().Where(m => m.Name == name).ToList();
+        if (methods.Count == 0)
+            throw new ArgumentException();
+        hlslName ??= methods[0].Name.ToLower();
+        var returnType = TypeResolver.CreateType(methods[0].ReturnType, _ => { });
+        foreach (var method in methods)
+        {
+            var arguments = new List<TypeSymbol>();
+            if (method.CallingConvention >= CallingConventions.HasThis)
+                arguments.Add(TypeResolver.CreateType(type, _ => { }));
+            var paramTypes = method.GetParameters().Select(x => x.ParameterType).ToList();
+            arguments.AddRange(paramTypes.Select(x => TypeResolver.CreateType(x, _ => { })));
+            var symbol = new IntrinsicFunctionSymbol(hlslName, returnType, arguments);
+            functions[($"{method.DeclaringType!.Name}.{method.Name}", symbol.ArgumentTypes.Length)] = symbol;
+        }
+    }
+
+    private static void CreateMemberCtorIntrinsic(Type type)
+    {
+        var constructors = type.GetConstructors() ?? throw new ArgumentException();
         var declaringType = constructors[0].DeclaringType!;
         var returnType = TypeResolver.CreateType(declaringType, _ => { });
         var hlslName = returnType.FullName.ToLower();
-        var arguments = new List<TypeSymbol>();
         foreach (var constructor in constructors)
         {
+            var arguments = new List<TypeSymbol>();
             var paramTypes = constructor.GetParameters().Select(x => x.ParameterType).ToList();
             // todo
             if (paramTypes.Contains(typeof(ReadOnlySpan<float>))
@@ -122,7 +125,7 @@ internal sealed class IntrinsicFunctionSymbol : FunctionSymbol
             //
             arguments.AddRange(paramTypes.Select(x => TypeResolver.CreateType(x, _ => { })));
             var symbol = new IntrinsicFunctionSymbol(hlslName, returnType, arguments);
-            functions[$"{declaringType.Name}..ctor"] = symbol;
+            functions[($"{declaringType.Name}..ctor", symbol.ArgumentTypes.Length)] = symbol;
         }
     }
 
@@ -133,8 +136,8 @@ internal sealed class IntrinsicFunctionSymbol : FunctionSymbol
         ArgumentTypes = argumentTypes.ToArray();
     }
 
-    public static IntrinsicFunctionSymbol Resolve(string name)
+    public static IntrinsicFunctionSymbol Resolve(string name, int argsCount)
     {
-        return functions[name];
+        return functions[(name, argsCount)];
     }
 }
