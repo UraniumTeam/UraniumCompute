2cbb10b128d048fe0a1c80c44929bbc10563780b
n-dub <ragucoz@gmail.com>
2022-12-23 19:40:46 +0500

    Add new tests

---
+++ ManagedSource/UraniumCompute/Tests/MathTests/RandomTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Numerics;
+using UraniumCompute.Common.Math;
+
+namespace MathTests;
+
+[TestFixture]
+public sealed class RandomTests
+{
+    [Test]
+    public void TestUnitCircle()
+    {
+        var random = new GpuRandom();
+        var previous = Vector2.One;
+        for (var i = 0; i < 100_000; i++)
+        {
+            var v = random.InsideUnitCircle();
+            Assert.That(Vector2.Distance(v, Vector2.Zero), Is.LessThanOrEqualTo(1.001f));
+            Assert.That(v, Is.Not.EqualTo(previous));
+            previous = v;
+        }
+    }
+
+    [Test]
+    public void TestUnitSphere()
+    {
+        var random = new GpuRandom();
+        var previous = Vector3.One;
+        for (var i = 0; i < 100_000; i++)
+        {
+            var v = random.InsideUnitSphere();
+            Assert.That(Vector3.Distance(v, Vector3.Zero), Is.LessThanOrEqualTo(1.001f));
+            Assert.That(v, Is.Not.EqualTo(previous));
+            previous = v;
+        }
+    }
+}
