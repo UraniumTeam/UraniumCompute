0472b9269de7c6d64ecdc27e08867c6c7c829640
n-dub <ragucoz@gmail.com>
2023-04-07 23:47:42 +0500

    Remove unused `JobInitializer` class

--- ManagedSource/UraniumCompute/UraniumCompute/Acceleration/Allocators/IDeviceAllocator.cs
+++ ManagedSource/UraniumCompute/UraniumCompute/Acceleration/Allocators/IDeviceAllocator.cs
@@ -3,15 +3,16 @@
 public interface IDeviceAllocator
 {
     public const ulong DefaultAlignment = 256;
-    
+
     ulong AllocatedByteCount { get; }
 
     void Init(in Desc desc);
     NullableHandle Allocate(ulong byteSize, ulong byteAlignment = 0);
     void DeAllocate(NullableHandle ptr);
     void GarbageCollect();
+    void GarbageCollectForce();
     void Reset();
-    
+
     public record struct Desc(NullableHandle AddressBase, ulong CapacityInBytes, ulong AlignmentInBytes, int GCLatency = 3)
     {
         public Desc(ulong capacityInBytes, int gcLatency = 3)
