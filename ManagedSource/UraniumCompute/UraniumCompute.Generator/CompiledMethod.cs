c4897af38f9e936041cae41ff9b703d8ab9b35cf
Dmitry Ishoev <26188825+NetVoobrazhenia@users.noreply.github.com>
2023-04-16 22:29:36 +0500

    Created a generator, an attribute and a project checking the generator

---
+++ ManagedSource/UraniumCompute/UraniumCompute.Generator/CompiledMethod.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace UraniumCompute.Generator;
+
+internal class CompiledMethod
+{
+    public MethodDeclarationSyntax Declaration { get; }
+    public string Name { get; }
+    public BlockSyntax Body { get; }
+
+    public CompiledMethod(MethodDeclarationSyntax method)
+    {
+        Declaration = method;
+        Name = method.Identifier.Text;
+        Body = method.Body!;
+    }
+}
