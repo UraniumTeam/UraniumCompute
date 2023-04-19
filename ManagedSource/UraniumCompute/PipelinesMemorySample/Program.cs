ff83a2d3dbdf85629322b1decc350cbb7f9134f6
n-dub <ragucoz@gmail.com>
2023-04-19 17:34:43 +0500

    Update Pipelines memory sample

--- ManagedSource/UraniumCompute/PipelinesMemorySample/Program.cs
+++ ManagedSource/UraniumCompute/PipelinesMemorySample/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Globalization;
+﻿using System.Diagnostics;
+using System.Globalization;
 using UraniumCompute.Acceleration;
 using UraniumCompute.Acceleration.Pipelines;
 using UraniumCompute.Acceleration.TransientResources;
@@ -7,7 +8,7 @@ using UraniumCompute.Compiler.InterimStructs;
 using var scheduler = JobScheduler.CreateForVulkan();
 using var pipeline = scheduler.CreatePipeline();
 
-var sourceData = Enumerable.Range(0, 1024).Select(x => (float)x).ToArray();
+var sourceData = Enumerable.Range(0, 2 * 1024 * 1024).Select(x => (float)(x % 16)).ToArray();
 
 var bufferA = TransientBuffer1D<float>.Null;
 var bufferB = TransientBuffer1D<float>.Null;
@@ -56,12 +57,28 @@ pipeline.AddDeviceJob("Sin(2x) + 500",
     }
 );
 
-Console.WriteLine("Waiting for tasks to complete...");
+Console.WriteLine("Waiting for tasks to complete on GPU...");
 var result = await pipeline.Run();
 Console.WriteLine($"Task was completed in {result.ElapsedMilliseconds}ms");
+
+var sw = new Stopwatch();
+var cpuResult = new float[sourceData.Length];
+Console.WriteLine("Waiting for tasks to complete on CPU...");
+sw.Start();
+for (var i = 0; i < cpuResult.Length; ++i)
+{
+    cpuResult[i] = MathF.Sin(2 * sourceData[i]) + 500;
+}
+sw.Stop();
+Console.WriteLine($"Task was completed in {sw.ElapsedMilliseconds}ms");
 
 using (var map = bufferD.Buffer.Map())
 {
     Console.WriteLine(
         $"Calculation results: [{string.Join(", ", map.Select(x => x.ToString(CultureInfo.InvariantCulture)).Take(32))}, ...]");
+
+    for (var i = 0; i < sourceData.Length; i++)
+    {
+        Trace.Assert(Math.Abs(map[i] - (MathF.Sin(sourceData[i] * 2) + 500)) < 0.0001f, $"index was {i}");
+    }
 }
