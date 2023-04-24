4157ac086a169f21f478e3d2d2e7a97892c5016d
n-dub <ragucoz@gmail.com>
2023-04-24 21:06:52 +0500

    Create AliasedResourceTracker class

--- ManagedSource/UraniumCompute/UraniumCompute/Acceleration/Pipelines/JobSetupContext.cs
+++ ManagedSource/UraniumCompute/UraniumCompute/Acceleration/Pipelines/JobSetupContext.cs
@@ -55,9 +55,8 @@ public abstract class JobSetupContext : IJobSetupContext
         where T : unmanaged
     {
         var buffer = Pipeline.GetTransientResourceHeap(resource.MemoryKindFlags)
-            .CreateBuffer1D(resource.Id, resource.Descriptor, out var info);
+            .CreateBuffer1D(resource.Id, resource.Descriptor, this);
         Pipeline.InitResource(resource.Id, buffer);
-        // TODO: place barrier
     }
 
     public IJobSetupContext ReadBuffer<T>(ITransientBuffer<T> buffer) where T : unmanaged
@@ -88,6 +87,11 @@ public abstract class JobSetupContext : IJobSetupContext
 
     public virtual void Init()
     {
+        foreach (var action in initActions)
+        {
+            action();
+        }
+
         foreach (var resource in variables)
         {
             if (resource.Deleter == ComputeJob)
@@ -95,11 +99,6 @@ public abstract class JobSetupContext : IJobSetupContext
                 Pipeline.GetTransientResourceHeap(resource.MemoryKindFlags)
                     .ReleaseResource(resource.Id);
             }
-        }
-
-        foreach (var action in initActions)
-        {
-            action();
         }
     }
 }
---
+++ ManagedSource/UraniumCompute/UraniumCompute/Acceleration/TransientResources/TransientResourceInfo.cs
@@ -0,0 +1,46 @@
+﻿using UraniumCompute.Acceleration.Allocators;
+using UraniumCompute.Acceleration.Pipelines;
+using UraniumCompute.Backend;
+
+namespace UraniumCompute.Acceleration.TransientResources;
+
+internal struct TransientResourceInfo
+{
+    public readonly NullableHandle Address;
+    public readonly BufferBase Resource;
+    public readonly IJobContext Creator;
+
+    public ulong MinOffset;
+    public ulong MaxOffset;
+
+    public TransientResourceInfo(BufferBase resource, IJobContext creator, NullableHandle address, ulong allocationSize)
+    {
+        Address = address;
+        Resource = resource;
+        Creator = creator;
+        MinOffset = (ulong)Address;
+        MaxOffset = MinOffset + allocationSize - 1;
+    }
+
+    internal Intersection Intersect(in TransientResourceInfo newResource)
+    {
+        if (MaxOffset < newResource.MinOffset || MinOffset > newResource.MaxOffset)
+        {
+            return Intersection.None;
+        }
+
+        if (MinOffset >= newResource.MinOffset && MaxOffset <= newResource.MaxOffset)
+        {
+            return Intersection.Full;
+        }
+
+        return Intersection.Partial;
+    }
+
+    internal enum Intersection
+    {
+        None,
+        Full,
+        Partial
+    }
+}
