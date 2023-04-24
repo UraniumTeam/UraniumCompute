5682b18778aff4f0f692207fae0066ed530f73a1
Dmitry Ishoev <26188825+NetVoobrazhenia@users.noreply.github.com>
2023-04-25 01:32:09 +0500

    Refactoring

--- ManagedSource/UraniumCompute/GeneratorSample/Program.cs
+++ ManagedSource/UraniumCompute/GeneratorSample/Program.cs
@@ -7,25 +7,22 @@ internal class Program
     [CompileKernel]
     public int SomeMethod1()
     {
-        return 0;
+        return 5+5;
     }
-
-    [CompileKernel]
+    
     public string SomeMethod2()
     {
         return "00";
     }
 
     [CompileKernel]
-    public string SomeMethod3()
+    public static string SomeMethod3()
     {
         return "0";
     }
 
     public static void Main(string[] args)
     {
-        TempGenerated.TempClass.Write();
-        //TempGenerated2
-        Console.WriteLine("He, World!");
+        Console.WriteLine("Hello, World!");
     }
 }
